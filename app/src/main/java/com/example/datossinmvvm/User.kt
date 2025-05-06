cee8052f3466effc259a6a4c76bf1b9310c4c4b9
Jean Benavente <jean.benavente@tecsup.edu.pe>
2025-05-05 22:31:20 -0500

    Add user

---
+++ app/src/main/java/com/example/datossinmvvm/User.kt
@@ -0,0 +1,8 @@
+package com.example.datossinmvvm
+
+@Entity
+data class User(
+    @PrimaryKey(autoGenerate = true) val uid: Int = 0,
+    @ColumnInfo(name = "first_name") val firstName: String?,
+    @ColumnInfo(name = "last_name") val lastName: String?
+)
---
+++ app/src/main/java/com/example/datossinmvvm/UserDao.kt
@@ -0,0 +1,10 @@
+package com.example.datossinmvvm
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM User")
+    suspend fun getAll(): List<User>
+
+    @Insert
+    suspend fun insert(user: User)
+}
